77d63874e956eb179143c66120003851d16e2a57
yu <oscarrr110@gmail.com>
2012-05-25 09:45:14 +0800

    add ch04

---
+++ src/main/java/ch04/TextArrayWritable.java
@@ -0,0 +1,12 @@
+package ch04;
+// == TextArrayWritable
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.Text;
+
+// vv TextArrayWritable
+public class TextArrayWritable extends ArrayWritable {
+  public TextArrayWritable() {
+    super(Text.class);
+  }
+}
+// ^^ TextArrayWritable
